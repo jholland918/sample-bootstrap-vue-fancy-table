31b73b110f8f36e9fe87611cee57bd20f7341040
DESKTOP-JR6P3DK\Jason <jholland918@gmail.com>
2020-12-27 18:39:22 -0600

    add search input

---
+++ src/components/fancy-tables/SearchInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search-input">
+    Search
+    <b-form-input v-model="text" placeholder="Enter search text"></b-form-input>
+  </div>
+</template>
+<script>
+export default {
+  name: "search-input",
+  data() {
+    return {
+      text: "",
+    };
+  },
+};
+</script>
+<style scoped>
+.search-input {
+  display: flex;
+  align-items: center;
+  max-width: 200px;
+}
+.search-input > input {
+    margin-left: 0.5rem;
+}
+</style>
---
+++ tests/components/fancy-tables/BaseTable.spec.js
@@ -0,0 +1,109 @@
+/// <reference types="cypress" />
+import Vue from 'vue';
+import '@/plugins/bootstrap-vue';
+import { mount } from '@cypress/vue';
+import BaseTable from '@/components/fancy-tables/base-table';
+import PageSizeSelect from '@/components/fancy-tables/PageSizeSelect';
+import SearchInput from '@/components/fancy-tables/SearchInput';
+Vue.component('base-table', BaseTable);
+Vue.component('page-size-select', PageSizeSelect);
+Vue.component('search-input', SearchInput);
+
+describe('Base Table', () => {
+    const items = [
+        { isActive: true, age: 40, first_name: "John", last_name: "Smith", },
+        { isActive: false, age: 21, first_name: "Larsen", last_name: "Shaw" },
+        { isActive: false, age: 89, first_name: "Geneva", last_name: "Wilson" },
+        { isActive: true, age: 38, first_name: "Jami", last_name: "Carney" },
+    ];
+
+    it('should render the table', () => {
+        mount({
+            template: `<base-table :items="items">
+                <div slot="default" slot-scope="{ items }">
+                    <b-table :items="items"></b-table>
+                </div>
+            </base-table>`,
+            data: function () {
+                return {
+                    items: items
+                }
+            }
+        });
+
+        cy.get('table').should('be.visible');
+    });
+
+    it('should render the page size select', () => {
+        mount({
+            template: `<base-table :items="items">
+                <div slot="default" slot-scope="{ items }">
+                    <page-size-select></page-size-select>
+                    <b-table :items="items"></b-table>
+                </div>
+            </base-table>`,
+            data: function () {
+                return {
+                    items: items
+                }
+            }
+        });
+
+        cy.get('.page-size-select').should('be.visible');
+        cy.get('.page-size-select > select').should('have.value', 10);
+    });
+
+    it('should render the pagination', () => {
+        mount({
+            template: `<base-table :items="items">
+                <div slot="default" slot-scope="{ items }">
+                    <b-table :items="items" :per-page="perPage" :current-page="currentPage"></b-table>
+                    <b-pagination
+                      v-model="currentPage"
+                      :total-rows="rows"
+                      :per-page="perPage"
+                      aria-controls="my-table"
+                    ></b-pagination>
+                </div>
+            </base-table>`,
+            data: function () {
+                return {
+                    perPage: 3,
+                    currentPage: 1,
+                    items: items
+                }
+            },
+            computed: {
+                rows() {
+                    return this.items.length
+                }
+            }
+        });
+
+        cy.get('.pagination.b-pagination').should('be.visible');
+        cy.get('.page-link').contains('2').should('be.visible');
+    });
+
+    it.only('should render the search input', () => {
+        mount({
+            template: `<base-table :items="items">
+                <div slot="default" slot-scope="{ items }">
+                    <search-input></search-input>
+                    <b-table :items="items"></b-table>
+                </div>
+            </base-table>`,
+            data: function () {
+                return {
+                    items: items
+                }
+            },
+            computed: {
+                rows() {
+                    return this.items.length
+                }
+            }
+        });
+
+        cy.get('.search-input').should('be.visible');
+    });
+});
