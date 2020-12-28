e457f7142610da9baf400c2dc6457fd86a5b31ce
DESKTOP-JR6P3DK\Jason <jholland918@gmail.com>
2020-12-28 17:44:40 -0600

    add vue i18n plugin

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  i18n,
+  render: h => h(App)
 }).$mount('#app')
