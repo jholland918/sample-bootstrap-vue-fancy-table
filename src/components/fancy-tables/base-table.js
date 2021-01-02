609a9c8c49bdebeb23e2f80523a79da61ab2bbdc
DESKTOP-JR6P3DK\Jason <jholland918@gmail.com>
2021-01-02 00:42:39 -0600

    did stuff

--- src/components/fancy-tables/base-table.js
+++ src/components/fancy-tables/base-table.js
@@ -1,6 +1,9 @@
 export default {
     name: 'base-table',
     props: {
+        fields: {
+            type: Array,
+        },
         items: {
             type: Array,
         },
@@ -26,8 +29,7 @@ export default {
             filterObj: {
                 value: ''
             },
-            /** Object indexed by field key with filter text for each column */
-            columnFilterModel: {},
+            isBusy: false
         };
     },
     computed: {
@@ -35,65 +37,61 @@ export default {
             return this.items.length;
         },
         columnFilter: function () {
-            if (!Object.values(this.columnFilterModel).some(v => {
+            let fields = this.fields.filter(f => {
                 // Intentionally skipping empty strings (""), nulls, undefined, and NaN values because we don't filter on those.
-                return v || v === false || v === 0;
-            })) {
-                return null;
-            }
+                return f.filter.model || f.filter.model === false || f.filter.model === 0;
+            });
 
-            return this.columnFilterModel;
+            return fields.length ? fields : null;
         }
     },
     render() {
         return this.$scopedSlots.default({
             getData: this.getData,
             items: this.items,
+            fields: this.fields,
+            isBusy: this.isBusy,
             totalRows: this.totalRows,
             currentPage: this.currentPageObj,
             perPage: this.perPageObj,
             filter: this.filterObj,
             columnFilter: this.columnFilter,
-            columnFilterModel: this.columnFilterModel,
             columnFilterFunc: this.columnFilterFunc,
         });
     },
     mounted() {
         this.table = this.$children.find(c => c.$el.classList.contains("b-table"));
-
-         this.table.computedFields.forEach((field) => {
-             this.$set(this.columnFilterModel, field.key, null);
-         });
-
         console.log('base-table mounted', this.table);
     },
     methods: {
-        columnFilterFunc(item, criteria) {
+        columnFilterFunc(item, fields) {
 
-            for (const property in criteria) {
-                if (!criteria.hasOwnProperty(property)) {
-                    continue;
-                }
+            let count = fields.length;
+            for (let i = 0; i < count; i++) {
+                let field = fields[i];
+                let filter = field.filter;
+                
+                console.log('filter');
 
-                let criterion = criteria[property];
-
-                // We can't just check for falsy (eg `!criterion`) before skipping because there are some falsy values that might be legitimate filter criteria.
-                if (criterion === '' || criterion === null || criterion === undefined) {
-                    continue;
-                }
-
-                if (!item.hasOwnProperty(property)) {
+                if (!item.hasOwnProperty(field.key)) {
                     return false;
                 }
 
-                let value = item[property];
+                let value = item[field.key];
 
-                if (criterion === value) {
-                    continue;
-                }
-
-                if (String(value).toLowerCase().indexOf(String(criterion).toLowerCase()) === -1) {
-                    return false;
+                switch (filter.type) {
+                    case 'select':
+                        if (filter.model !== value) {
+                            return false;
+                        }
+                        break;
+                
+                    default:
+                        // Assume text
+                        if (String(value).toLowerCase().indexOf(String(filter.model).toLowerCase()) === -1) {
+                            return false;
+                        }
+                        break;
                 }
             }
 
